ae1c96d7d552e859598a0810bb5ef53a595e0b10
Christopher Eyre <chriseyre2000@gmail.com>
2019-05-05 19:39:10 +0100

    At end of chapter 7 (but is broken)

--- mastery/lib/mastery.ex
+++ mastery/lib/mastery.ex
@@ -3,10 +3,6 @@ defmodule Mastery do
   alias Mastery.Boundary.{TemplateValidator, QuizValidator}
   alias Mastery.Core.Quiz
 
-  def start_quiz_manager() do
-    GenServer.start_link(QuizManager, %{}, name: QuizManager)
-  end
-
   def build_quiz(fields) do
     with :ok <- QuizValidator.errors(fields),
          :ok <- GenServer.call(QuizManager, {:build_quiz, fields}),
@@ -21,19 +17,19 @@ defmodule Mastery do
 
   def take_quiz(title, email) do
     with %Quiz{} = quiz <- QuizManager.lookup_quiz_by_title(title),
-         {:ok, session} <- GenServer.start_link(QuizSession, {quiz, email})
+         {:ok, _} <- GenServer.start_link(QuizSession, {quiz, email})
     do
-      session
+      {title, email}
     else
       error -> error  
     end
   end
 
-  def select_question(session) do
-    GenServer.call(session, :select_question)
+  def select_question(name) do
+    GenServer.call(name, :select_question)
   end
 
-  def answer_question(session, answer) do
-    GenServer.call(session, {:answer_question, answer})
+  def answer_question(name, answer) do
+    GenServer.call(name, {:answer_question, answer})
   end
 end
--- mastery/lib/mastery/application.ex
+++ mastery/lib/mastery/application.ex
@@ -9,11 +9,18 @@ defmodule Mastery.Application do
     IO.puts "starting Mastery"
     # List all child processes to be supervised
     children = [
-      { Mastery.Boundary.QuizManager, 
+      { 
+        Mastery.Boundary.QuizManager, 
         [name: Mastery.Boundary.QuizManager]
+      },
+      { 
+        Registry,
+        [name: Mastery.Registry.QuizSession, keys: :unique]
+      },
+      {
+        DynamicSupervisor,
+        [name: Mastery.Supervisor.QuizSession, strategy: :one_for_one]
       }
-      # Starts a worker by calling: Mastery.Worker.start_link(arg)
-      # {Mastery.Worker, arg}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
